da422d2f13bc704cb3823fe6d2c7ef6bb3539c27
Eobam <elliottlieberman123@gmail.com>
2026-01-07 22:58:56 -0500

    basic rsvp stuff

--- static/main.js
+++ static/main.js
@@ -1,12 +1,16 @@
 const form = document.getElementById("rsvpForm")
-const rvpsContainer = document.getElementById("rsvps");
+const rsvpsContainer = document.getElementById("rsvps");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-});
 
-const name = form.elements.name.value;
-const email = form.elements.email.value;
 
-const item = document.createElement("p");
-item.textContent = ''
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+
+    const item = document.createElement("p");
+    item.textContent = 'test'
+    rsvpsContainer.appendChild(item);
+
+    form.reset();
+})
