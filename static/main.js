00bc4a0b173ac48b68c9086f7af3e12490661cf8
Eobam <elliottlieberman123@gmail.com>
2026-01-07 22:31:00 -0500

    js

--- static/main.js
+++ static/main.js
@@ -1,1 +1,12 @@
-const form - document.getElementById("rsvpForm")
+const form = document.getElementById("rsvpForm")
+const rvpsContainer = document.getElementById("rsvps");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
+
+const name = form.elements.name.value;
+const email = form.elements.email.value;
+
+const item = document.createElement("p");
+item.textContent = ''
